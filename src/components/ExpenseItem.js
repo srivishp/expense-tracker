347024d5f85ff9ea320c8b8f78515a03b4570447
Sri Vishnu <srivishp@gmail.com>
2023-08-07 20:28:23 +0530

    Created date component and added CSS

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,11 @@
 import "./componentStyles/ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem({ date, title, amount }) {
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      {/* This variable "dateProps" can be named anything  */}
+      <ExpenseDate dateProps={date} />
       <div className="expense-item__description">
         <h2 className="expense-item h2">{title}</h2>
         <div className="expense-item__price">₹{amount}</div>
